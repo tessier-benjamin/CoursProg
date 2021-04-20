4b984780e66f552f2e6faf074d43f944a7fdbe8a
tessier-benjamin <52489423+tessier-benjamin@users.noreply.github.com>
2021-04-20 10:54:40 +0200

    Add files via upload

--- coursProg/src/coursProg/visite.java
+++ coursProg/src/coursProg/visite.java
@@ -13,13 +13,48 @@ public class visite {
 		this.note=note;
 	}
 	
+	String getEtudiant()
+	{
+		return this.etudiant;
+	}
+	
+	String getDate()
+	{
+		return this.date;
+	}
+	
+	int getNote()
+	{
+		return this.note;
+	}
+	
+	void setEtudiant(String etudiant)
+	{
+		this.etudiant=etudiant;
+	}
+	
+	void setDate(String date)
+	{
+		this.date=date;
+	}
+	
+	void setNote(int note)
+	{
+		this.note=note;
+	}
+	
+	public String toString() 
+	{
+		return "date :" + this.date +"  etudiant :" + this.etudiant +" note :" + this.note;
+	}
+	
 	public static void  main(String[] args ) 
 	{
 		visite v1= new visite("","",0);
 		visite v2= new visite("porto","17-08-1592",15);
 		
-		System.out.println(v1.etudiant);
-		System.out.println(v2.etudiant);
+		System.out.println(v1);
+		System.out.println(v2);
 	}
 	
 }
